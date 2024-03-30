40d80963999502bd018e91b7a7757f9de64dc74d
nasteexocali <165520335+nasteexocali@users.noreply.github.com>
2024-03-31 01:05:35 +0300

    Add files via upload
    Nasteha Ali Ahmed

---
+++ Personaal-Portofolio/src/Componts/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className='container'>
+    
+      <section className="hero-section">
+          <h2>
+        
+           <img className='imgs' src="./public/image012.png" alt=""/>
+            
+            <br />
+           
+            I'm Nasteha ,
+            <br />
+          Markating
+          </h2>
+          <p>
+            Your Are 7 Years Exprense  Web Devalober Vision into Reality:
+            Innovative Digital Markating
+            by a Professional Markating And HR!
+          </p>
+          <button>Start Project</button>
+        </section>
+    </div>
+  )
+}
